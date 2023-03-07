59d88e8bb3a912408243fee5ad7e64c0959d7c61
박현우 <a40293865@gmail.com>
2023-03-08 01:39:36 +0900

    Links Component를 Span에 a 태그를 넣어서 잉여 margin을 없애볼까 고민중

--- gsap_practice/src/components/body/contents/Projects/DolBom.tsx
+++ gsap_practice/src/components/body/contents/Projects/DolBom.tsx
@@ -1,5 +1,6 @@
 import * as Styled from '../../styled';
 import { Links } from '../../common/Links';
+import GitHubIcon from '@mui/icons-material/GitHub';
 export const DolBom = () => {
   return (
     <>
@@ -9,8 +10,16 @@ export const DolBom = () => {
         <Styled.CardExPlain>✔ 상담사를 위한 비대면 상담플랫폼 개발</Styled.CardExPlain>
         <Styled.CardExPlain>✔ 캘린더 기능과 드라이브 기능 개발 담당</Styled.CardExPlain>
         <Links
-          text="예시"
+          text="돌봄 프로젝트 GitHub"
+          href="https://github.com/dailybetter/DOLBOMpjt"
+        />
+        <Links
+          text="돌봄 프로젝트 전반적 회고 "
           href="https://dailybetter.github.io/retrospective/projectRet/"
+        />
+        <Links
+          text="프로젝트를 진행하며 고민했던 것 들 "
+          href="https://dailybetter.github.io/retrospective/pjtRet/"
         />
       </Styled.CardExplainContainer>
     </>
